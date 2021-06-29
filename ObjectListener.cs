866389fcb302796e2bce93561ef42f46bcc49d06
Kiokuu <78288562+Kiokuu@users.noreply.github.com>
2021-06-29 21:32:32 +0100

    warning spam go away!

--- ObjectListener.cs
+++ ObjectListener.cs
@@ -1,4 +1,6 @@
-﻿namespace NameplateStats
+﻿using UnhollowerBaseLib.Attributes;
+
+namespace NameplateStats
 {
     using System;
     using UnityEngine;
@@ -6,10 +8,13 @@
     public class ObjectListener : MonoBehaviour
     {
         public ObjectListener(IntPtr ptr) : base(ptr) {}
-
+        [method: HideFromIl2Cpp]
         public event Action OnEnableEvent;
+        [method: HideFromIl2Cpp]
         public event Action OnDisableEvent;
+        [method: HideFromIl2Cpp]
         private void OnEnable() => OnEnableEvent?.Invoke();
+        [method: HideFromIl2Cpp]
         private void OnDisable() => OnDisableEvent?.Invoke();
     }
 }
