70bb2f9543d3499699a80cf9f698801587df40d0
Kiokuu <78288562+Kiokuu@users.noreply.github.com>
2021-08-05 00:05:37 +0100

    ver change for u2019 recompile

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using HarmonyLib;
 using MelonLoader;
 
 [assembly: MelonGame("VRChat","VRChat")]
-[assembly: MelonInfo(typeof(NameplateStats.Main),"NameplateStats","1.0.4","Yato + Dawn","https://www.github.com/Kiokuu/NameplateStats")]
+[assembly: MelonInfo(typeof(NameplateStats.Main),"NameplateStats","1.0.5","Yato#4499 + Dawn","https://www.github.com/Kiokuu/NameplateStats")]
 [assembly: MelonColor(ConsoleColor.DarkCyan)]
-[assembly: MelonPriority(Priority = Priority.LowerThanNormal)]
+[assembly: MelonPriority(62)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
